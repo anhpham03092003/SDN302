0612168016deba2a3a08b7c25776e3f0010edeb9
HaoTH <haothhe176192@fpt.edu.vn>
2024-10-04 13:54:55 +0700

    Fix<index>

--- sdn302/src/index.js
+++ sdn302/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
+
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,14 +15,9 @@ root.render(
  
     </BrowserRouter>
   </React.StrictMode>
-=======
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
->>>>>>> f38dafd4ff833c06d0d3d634da5e7acd9cb464cb
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
