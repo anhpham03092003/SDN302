d88be67e9e6f6b53f5e79d640b860b34e4aefd0f
TriNM <nguyentri16112003@gmail.com>
2024-10-27 14:18:07 +0700

    FE: connect: task list, create task

--- sdn302/src/Components/Group_Components/GroupColumn.jsx
+++ sdn302/src/Components/Group_Components/GroupColumn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import { FaCheck, FaPlus } from 'react-icons/fa';
 import { FaRegTrashCan } from 'react-icons/fa6';
@@ -6,12 +6,25 @@ import { IoMdClose, IoMdMore } from "react-icons/io";
 import GroupTask from './GroupTask';
 import GroupTaskDetail from './GroupTaskDetail';
 import { IoCheckmark } from 'react-icons/io5';
+import { AppContext } from '../../Context/AppContext';
+import axios from 'axios';
 function GroupColumn({column}) {
+    const {groups_API,group,setGroup,accessToken} = useContext(AppContext)
     const [show, setShow] = useState(false);
+    const tasks = group.tasks.filter((t)=>{
+        return t.status == column
+    });
+    const [newTask,setNewTask] = useState("");
     const handleRemoveTask = () => {
         if (window.confirm("Remove this column?")) {
 
         }
+    }
+    async function handleCreateTask (){
+        await axios.post(`${groups_API}/${group._id}/tasks/create`,{taskName:newTask,status:column},{headers:{ Authorization: `Bearer ${accessToken}`}})
+        .then((res)=>{setNewTask('');setAddTask(false)})
+        .catch((err)=>console.log(err));
+
     }
     const [editColumn, setEditColumn] = useState(false)
     const [addTask, setAddTask] = useState(false)
@@ -21,7 +34,7 @@ function GroupColumn({column}) {
                 <Col md={10} className='text-start'>
                     {editColumn == false ? <p className='m-0 p-1 rounded-1 background-hover fw-bold' onClick={() => setEditColumn(true)}>{column}</p> :
                         <Row className='d-flex justify-content-between'>
-                            <Col md={8}><input type="text" name='columnName' className='w-100' required/></Col>
+                            <Col md={8}><input type="text" name='columnName' className='w-100 m-0' required/></Col>
                             <Col md={2} className='background-hover bg-white border border-1 border-black' onClick={() => { setEditColumn(false) }}><IoCheckmark /></Col>
                             <Col md={2} className='background-hover bg-white border border-1 border-black' onClick={() => { setEditColumn(false) }}><IoMdClose /></Col>
 
@@ -30,20 +43,22 @@ function GroupColumn({column}) {
                 <Col md={2} className='text-end item-hover p-1'><IoMdMore /></Col>
             </Row>
 
-            <Row className='my-2' onClick={() => { setShow(true) }}>
-                <GroupTask />
-            </Row>
-
-            <Row className='my-2' onClick={() => { setShow(true) }}>
-                <GroupTask />
-            </Row>
+            {tasks?.map((task)=>{
+                return (<Row className='my-2' onClick={() => { setShow(true) }}>
+                            <GroupTask task={task} />
+                        </Row>)
+            })}
 
             <Row className='my-2'>
                 <Col md={9} className='text-start p-1'>
                     {addTask == false ? <p className='m-0 p-1 rounded-1 background-hover' onClick={() => setAddTask(true)}><FaPlus />  Create task</p> :
                         <Row className='d-flex justify-content-between'>
-                            <Col md={8}><input type="text" name='taskName' className='w-100' required/></Col>
-                            <Col md={2} className='background-hover bg-white border border-1 border-black' onClick={() => { setAddTask(false) }}><IoCheckmark /></Col>
+                            <Col md={8}><input type="text" name='taskName' className='w-100 m-0'
+                                onChange={(e)=>{setNewTask(e.target.value)}} required/></Col>
+                            <Col md={2} className='background-hover bg-white border border-1 border-black' 
+                                onClick={handleCreateTask}
+                            ><IoCheckmark /></Col>
+                            
                             <Col md={2} className='background-hover bg-white border border-1 border-black' onClick={() => { setAddTask(false) }}><IoMdClose /></Col>
 
                         </Row>}
--- sdn302/src/Components/Group_Components/GroupTask.jsx
+++ sdn302/src/Components/Group_Components/GroupTask.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import { FaCheckSquare, FaPen, FaRegCheckSquare } from 'react-icons/fa'
 
-function GroupTask() {
+function GroupTask({task}) {
     return (
         <Container className='bg-white p-3 cursor-pointer shadow'>
             <Row>
-                <Col md={9} className='text-start'><p className='m-0'>Task Name</p></Col>
+                <Col md={9} className='text-start'><p className='m-0'>{task.taskName}</p></Col>
                 <Col md={3} className='text-end'><FaPen className='item-hover'/></Col>
             </Row>
             <Row>
