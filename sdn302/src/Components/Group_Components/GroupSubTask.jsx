f1b15c4ac1bf4f1c2d488af2c3a9ab542c066043
TriNM <nguyentri16112003@gmail.com>
2024-10-28 15:23:26 +0700

    feat: BE-FE: Task CRUD

--- sdn302/src/Components/Group_Components/GroupSubTask.jsx
+++ sdn302/src/Components/Group_Components/GroupSubTask.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Col, Dropdown, Row } from 'react-bootstrap'
 import { FaClock, FaPen, FaRegClock, FaUser } from 'react-icons/fa'
 import { FaRegSquareCheck, FaRegTrashCan, FaSquareCheck } from 'react-icons/fa6'
 import { GrTasks } from 'react-icons/gr'
 import { IoMenu } from 'react-icons/io5'
+import { AppContext } from '../../Context/AppContext'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 function GroupSubTask({subTask}) {
+  const { groupId } = useParams();  
+  const {selectedTask, setSelectedTask, groups_API, group, setGroup, accessToken, groupMembers, setGroupMembers}= useContext(AppContext)
   const [isDone, setIsDone] = useState(false);
-  const handleRemoveSubTask = () => {
+  const handleRemoveSubTask = async () => {
     if (window.confirm("Remove this subtask?")) {
-
+      await axios.delete(`${groups_API}/${groupId}/tasks/${selectedTask?._id}/subTasks/${subTask._id}/delete`, { headers: { Authorization: `Bearer ${accessToken}` } })
+      .then((res) => {
+        const updatedSubTasks = selectedTask.subTasks.filter(subTask=>subTask._id!=res.data);
+        const updatedTasks = group.tasks.map(task =>
+            task._id == selectedTask._id ? { ...task, subTasks: updatedSubTasks } : task
+        );
+        const updatedGroup = { ...group, tasks: [...updatedTasks] };
+        setSelectedTask({...selectedTask,subTasks:[...updatedSubTasks]})
+        setGroup(updatedGroup)
+      })
+      .catch((err) => console.error(err));
     }
   }
   return (
@@ -31,7 +46,7 @@ function GroupSubTask({subTask}) {
             <Dropdown.Item eventKey="3">...</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
-        {isDone == true ? <FaRegSquareCheck className='mx-1 item-hover' onClick={() => { setIsDone(!isDone) }} /> : <FaSquareCheck className='mx-1 item-hover' onClick={() => { setIsDone(!isDone) }} />}
+        {subTask.status == "done" ? <FaRegSquareCheck className='mx-1 item-hover ' onClick={() => { setIsDone(!isDone) }} /> : <FaSquareCheck className='mx-1 item-hover text-success' onClick={() => { setIsDone(!isDone) }} />}
         <FaRegTrashCan className='mx-1 item-hover' onClick={handleRemoveSubTask} />
       </Col>
     </Row>
